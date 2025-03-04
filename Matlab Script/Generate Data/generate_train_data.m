6240545d79aa1682fa65918ffdef5c079e4c5fca
IceySwan <hi@icey.one>
2025-03-04 22:52:20 +0800

    Remove unused code

--- Matlab Script/Generate Data/generate_train_data.m
+++ Matlab Script/Generate Data/generate_train_data.m
@@ -1,6 +1,6 @@
 % 定义 x 和 t 的取值范围
 x0 = -5; x1 = 5;  % x 的范围
-t0 = -0.5; t1 = 0.5;  % t 的范围
+t0 = -3; t1 = 3;  % t 的范围
 
 % 定义分辨率
 x_points = 513;  % x 方向的点数
@@ -11,8 +11,15 @@ x = linspace(x0, x1, x_points);
 t = linspace(t0, t1, t_points);
 [X, T] = meshgrid(x, t);
 
-% 计算 q(x,t)
-q = 2 * sech(8 * T + 2 * X) .* exp(-2i * X + 1i);
+% 定义 A 和 B
+C = -0.246i*exp(-1i*(-0.64*T + 1.4i*X)) + 0.462i * exp(-1i*(-1.96*T - 0.8i*X))...
+    -0.264i*exp(1i*(0.64*T + 1.4i*X)) + 0.462i * exp(-1i*(-1.96*T + 0.8i * X));
+
+D = 1.21*exp(-0.6*X) + 1.21*exp(0.6*X) - 1.12*exp(1.32i*T)...
+    + 0.09*exp(2.2*X) - 1.12*exp(-1.32i*T) + 0.09*exp(-2.2*X);
+
+% 定义 q(x, t)
+q = -2i * C ./ D;
 % 分离实部虚部
 u = real(q);
 v = imag(q);
